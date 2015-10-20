6d9425777436e8b076c85758b09eb671ae2d302f
phillipwstewart <phillipwstewart@gmail.com>
2015-10-19 21:08:23 -0700

    Pretty much done with the test cases.
    We need to go through and review everything.
    We still need multithreaded tests for the command manager.
    Specifically, look for TODO comments...

--- Command_Manager_Test.java
+++ Command_Manager_Test.java
@@ -49,6 +49,22 @@
  */
 
 /* Test case M-03:
+ *  Enqueue_Command with valid command
+ *  Description:
+ *    Enqueue a valid Command object
+ *  Expected result:
+ *    The command should be enqueued
+ */
+
+/* Test case M-04:
+ *  Enqueue_Command with null
+ *  Description:
+ *    Attempt to Enqueue null.
+ *  Expected result:
+ *    Null_Object_Exception
+ */
+
+/* Test case M-05:
  *  Next_Command with enqueued command
  *  Description:
  *    Enqueue a valid Command object
@@ -57,7 +73,7 @@
  *    The enqueued command should be returned.
  */
 
-/* Test case M-04:
+/* Test case M-06:
  *  Execute_Next_Command with empty queue
  *  Description:
  *    Call Execute_Next_Command before enqueueing any commands.
@@ -65,85 +81,128 @@
  *    Null_Unit_ID_Exception <- should really be Null_Obj..
  */
 
-/* Test case M-04:
- *  Execute_Next_Command with 
+/* Test case M-07:
+ *  Execute_Next_Command with Toggle_Emergency_Command enqueued true->false
  *  Description:
- *    Call 
+ *    Create a Response_Unit with Emergency_Exists == true.
+ *    Add Response_Unit to the Response_Unit_DB.
+ *    Create a Toggle_Emergency_Command with the unit's Unit_ID.
+ *    Enqueue the command.
+ *    Call Execute_Next_Command.
+ *    Verify the command was executed.
+ *  Expected result:
+ *    Response_Unit Emergency_Exists should toggle to false.
+ */
+
+/* Test case M-08:
+ *  Execute_Next_Command with Toggle_Emergency_Command enqueued false->true
+ *  Description:
+ *    Create a Response_Unit with Emergency_Exists == false.
+ *    Add Response_Unit to the Response_Unit_DB.
+ *    Create a Toggle_Emergency_Command with the unit's Unit_ID.
+ *    Enqueue the command.
+ *    Call Execute_Next_Command.
+ *    Verify the command was executed.
+ *  Expected result:
+ *    Response_Unit Emergency_Exists should toggle to true.
+ */
+
+/* Test case M-09:
+ *  Execute_Next_Command with Set_Unit_Status_Command enqueued.
+ *  Description:
+ *    Create a Response_Unit, add it to the Response_Unit_DB.
+ *    Create a Set_Unit_Status_Command with the unit's Unit_ID.
+ *    Enqueue the command.
+ *    Call Execute_Next_Command.
+ *    Verify the command was executed.
+ *  Expected result:
+ *    Response_Unit Status should update.
+ */
+
+/* Test case M-10:
+ *  Execute_Next_Command with Set_Unit_Location_Command enqueued.
+ *  Description:
+ *    Create a Response_Unit, add it to the Response_Unit_DB.
+ *    Create a Set_Unit_Location_Command with the unit's Unit_ID.
+ *    Enqueue the command.
+ *    Call Execute_Next_Command.
+ *    Verify the command was executed.
+ *  Expected result:
+ *    Response_Unit Location should toggle.
+ */
+
+/* Test case M-11:
+ *  Execute_Next_Command with Send_Message_To_Operator_Command enqueued.
+ *  Description:
+ *    Create a Response_Unit, add it to the Response_Unit_DB.
+ *    Create a Send_Message_To_Operator_Command with the unit's Unit_ID.
+ *    Enqueue the command.
+ *    Call Execute_Next_Command.
+ *    Verify the command was executed.
+ *  Expected result:
+ *    Until fully implemented, should write message to stdout.
+ */
+
+/* Test case M-12:
+ *  Queue_Empty with empty queue
+ *  Description:
+ *    Call Queue_Empty with an empty queue
+ *  Expected result:
+ *    true
+ */
+
+/* Test case M-13:
+ *  Queue_Empty with non-empty queue
+ *  Description:
+ *    Call Queue_Empty with an non-empty queue
+ *  Expected result:
+ *    false
+ */
+
+/* Test case M-14:
+ *  Priority test with 1 of each priority Command (in order)
+ *  Description:
+ *    Enqueue a valid Toggle_Emergency_Command. (Priority == 1)
+ *    Enqueue a valid Set_Unit_Location_Command. (Priority == 2)
+ *    Enqueue a valid Send_Message_To_Operator_Command. (Priority == 3)
+ *    Call Next_Command 3 times and check priority of each.
+ *  Expected result:
+ *    Priority 1, then 2, then 3 returned in that order.
+ */
+
+/* Test case M-15:
+ *  Priority test with 1 of each priority Command (reverse order)
+ *  Description:
+ *    Enqueue a valid Send_Message_To_Operator_Command. (Priority == 3)
+ *    Enqueue a valid Set_Unit_Location_Command. (Priority == 2)
+ *    Enqueue a valid Toggle_Emergency_Command. (Priority == 1)
+ *    Call Next_Command 3 times and check priority of each.
+ *  Expected result:
+ *    Priority 1, then 2, then 3 returned in that order.
+ */
+
+
+
+/* Test case M-??:
+ *  Priority test with same priority?
+ *  Description:
+ *    don't know if we need to test this...
  *  Expected result:
  *    
  */
 
-/* Test case M-04:
- *  Queue_Empty with 
+
+//We should test with threads, since the project description mentions threads.
+//The problem is how do we verify across threads that this is working??
+//And we should be able to break it. PriorityQueue is not thread-safe.
+
+/* Test case M-??:
+ *  Priority test with multiple threads...
  *  Description:
- *    Call 
+ *    don't know...
  *  Expected result:
- *    
- */
-
-/* Test case M-04:
- *  Queue_Empty with 
- *  Description:
- *    Call 
- *  Expected result:
- *    
- */
-
-
-/* Test case M-05:
- *  Priority test with 1 each type Command
- *  Description:
- *    Enqueue
- *    Call Next_Command before enqueueing any commands.
- *  Expected result:
- *    null
- */
-
-/* Test case M-05:
- *  Priority test with 1 each type Command
- *  Description:
- *    Enqueue
- *    Call Next_Command before enqueueing any commands.
- *  Expected result:
- *    null
+ *    Highest priority always returned first.
  */
 
 
 
-
-// ORIGINAL CODE FOR UNIT IS INCLUDED BELOW FOR QUICK REFERENCE.
-// IT HAS BEEN STRIPPED OF EXTRANEOUS COMMENTS AND FORMATTED FOR CLARITY.
-// ENSURE THIS HAS BEEN DELETED BEFORE SUBMITTING ASSIGNMENT.
-
-/**
- * This class manages the queue of commands to be executed
- * highest priority is returned first
- * -or oldest highest is returned first.
- * 
- * Throws:
- *  Null_Unit_ID_Exception
- */
-public class Command_Manager {
-    private PriorityQueue <Command> Queue = new PriorityQueue <> (16, new Command_Comparator ());
-    
-    public Command Next_Command () {
-        return Queue.poll();
-    }
-    
-    public void Enqueue_Command (Command New_Command) {
-        Queue.offer(New_Command);
-    }
-
-    /**
-     * This subprogram removes and executes the next command on the queue.
-     * @throws Null_Unit_ID_Exception 
-     */
-    public void Execute_Next_Command () throws Null_Unit_ID_Exception {
-        //next_command() returns null if q empty...
-        Next_Command().Execute();
-    }
-
-    public Boolean Queue_Empty (){
-        return Queue.size() <= 0;
-    }
-} // end Command_Manager
